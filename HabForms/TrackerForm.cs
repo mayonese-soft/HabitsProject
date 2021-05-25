34758f73c63dee5b2e50b2b4c0c43bae7b7cf5a2
Barsik Kato <noran-kotov@yandex.ru>
2021-05-25 13:02:45 +0300

    НАДО ЗАСТАВИТЬ РАБОТАТЬ ЛИСТБОКС

--- HabForms/TrackerForm.cs
+++ HabForms/TrackerForm.cs
@@ -13,19 +13,41 @@ namespace HabForms
 {
     public partial class TrackerForm : Form
     {
+        List<Habits> habits = new List<Habits>();
+
         public TrackerForm()
         {
             InitializeComponent();
-            string[] activities;
-            List<Habits> habits = new List<Habits>();
-            using (StreamReader sr = new StreamReader("userData.csv"))
+            LoadHabitList();
+            LoadActivities();
+        }
+
+        private void LoadHabitList()
+        {
+            using (StreamReader sr = new StreamReader("habitData.csv"))
             {
                 while (sr.EndOfStream != true)
                 {
                     string[] str = sr.ReadLine().Split(';');
-                    habits.Add(new Habits() { hab_numb = Convert.ToInt32(str[0]), name = str[1], question = str[2], isGood = Convert.ToBoolean(str[3])});
+                    habits.Add(new Habits() { hab_numb = Convert.ToInt32(str[0]), name = str[1], question = str[2], isGood = Convert.ToBoolean(str[3]) });
                 }
             }
+        }
+
+        private void LoadActivities()
+        {
+            int i = 0;
+            string[] activities = new string[habits.Count];
+            foreach (Habits habit in habits)
+            {
+                activities[i] = habit.name;
+            }
+            var source = new AutoCompleteStringCollection();
+            source.AddRange(activities);
+            foreach (object obj in source)
+            {
+                listBox1.Items.Add(obj);
+            } 
         }
 
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
