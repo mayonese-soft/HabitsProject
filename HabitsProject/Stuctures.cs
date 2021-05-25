c0a93b74d803aebc813cc938308f9ee954ea244c
Helium Inert <helium.inert@outlook.com>
2021-05-25 08:34:35 +0300

    создала структуры привычек

---
+++ HabitsProject/Stuctures.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HabitsProject
+{
+    struct Habits // структура для полезных и вредных привычек
+    {
+        int hab_numb;
+        string quastion;
+        int answer;
+    }    
+    struct DaylyHabits // структура для ежедвевных привычек
+    {
+        int hab_numb;
+        bool did_or_not;
+    }
+}
