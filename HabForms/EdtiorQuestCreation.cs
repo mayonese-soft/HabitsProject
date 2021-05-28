2a657e82deb158b1d0c87f3d5ec7ac3de202bd55
Майоров Михаил <m.maiorov@inbox.ru>
2021-05-28 17:56:39 +0300

    Почти редактор сделали

--- HabForms/EdtiorQuestCreation.cs
+++ HabForms/EdtiorQuestCreation.cs
@@ -7,14 +7,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.IO;
 namespace HabForms
 {
+    
     public partial class EdtiorQuestCreation : Form
     {
         public EdtiorQuestCreation()
         {
             InitializeComponent();
+        }
+
+        private void questCreateButton_Click(object sender, EventArgs e)
+        {
+            File.Create(@"../../Quests/" + textBoxQuestName.Text + ".txt").Close();
         }
     }
 }
